551cbb05bc92ff5518aafa0177c45cbcae5efb19
이소정 <sjlee950326@gmail.com>
2021-07-15 20:22:40 +0900

    :zap: test client-environment

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,33 @@
 // import { getAllInfo } from './app';
 
+let state = {
+     value: ''
+};
 
 function render() {
-const root = document.getElementById("root");
-// const div = document.createElement("div");
-const input = document.createElement("input");
-const button = document.createElement("button");
+     const root = document.getElementById("root");
+     // const div = document.createElement("div");
+     const input = document.createElement("input");
+     const button = document.createElement("button");
 
-//  div.innerHTML = `${getAllInfo("자바스크립트")}`;
-button.innerText = '버튼 클릭!';
+     root.appendChild(input);
+     root.appendChild(button);
 
-root.appendChild(input);
-root.appendChild(button);
+     //  div.innerHTML = `${getAllInfo("자바스크립트")}`;
+     button.innerText = '버튼 클릭!';
+     button.value = button.nodeName;
+
+     button.onclick = e => handleClick(e.target.value.toLowerCase());
+
+     input.addEventListener('keydown', e => handleChange(e.target.value));
 }
 
-window.addEventListener("onLoad", render());
+function handleChange(e) {
+     console.log(e);
+}
+
+function handleClick(e) {
+     console.log(' success click');
+}
+
+window.addEventListener('onLoad', render());
